bfe6e272c3c2c106dba3e4f6fd36a2f61f45ec34
Jake Cermak <cerjakt@dunwoody.edu>
2020-05-16 20:57:24 -0500

    added new classes

---
+++ Project_Three_GUI/Models/Athlete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Three_GUI.Models
+{
+    class Athlete : Resident
+    {
+		public double boarding_fee { get; set; }
+
+		public Athlete(string name, int id, int room_number, double rent_fee, int floor, double boarding_fee)
+		{
+			this.name = name;
+			this.id = id;
+			this.room_number = room_number;
+			this.rent_fee = rent_fee;
+			this.floor = floor;
+			this.boarding_fee = boarding_fee;
+		}
+
+		//Define a toString method 
+		public override string ToString()
+		{
+			return String.Format($"Welcome, {name}, ID: {id}, Room Number: {room_number}, Rent Free: {rent_fee}, Floor Number: {floor}");
+		}
+
+	}
+}

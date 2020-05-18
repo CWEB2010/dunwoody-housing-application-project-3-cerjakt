335be02e7390f388c1bdeebcef016b31fc252d92
Jake Cermak <cerjakt@dunwoody.edu>
2020-05-18 00:06:36 -0500

    added credentials logic

--- Project_Three_GUI/MainWindow.xaml.cs
+++ Project_Three_GUI/MainWindow.xaml.cs
@@ -20,18 +20,30 @@ namespace Project_Three_GUI
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		//Initializes window at program start
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
+		//Window exits when the exit button is pressed
 		private void exit_btn(object sender, RoutedEventArgs e)
 		{
 			this.Close();
 		}
+
+		//Username and password logic that allows entry if they are correct
 		private void sbmt_btn(object sender, RoutedEventArgs e)
 		{
-			this.Close();
+			if (usernameBox.Text == "home")
+			{
+				if (passwordBox.Text == "1234")
+				{
+					SecondWindow ResidentWin = new SecondWindow();
+					ResidentWin.Show();
+					this.Close();
+				}
+			}
 		}
 	}
 }
